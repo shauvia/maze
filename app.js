f7cdf5db2293b9b64e970d4bbcfc48cf8a9b1fa2
shauvia <e.szalwinska@gmail.com>
2021-01-21 19:43:17 -0800

    all finished

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function Field(is_wall, is_entrance, is_exit){
   this.is_wall = is_wall;
   this.is_entrance = is_entrance;
   this.is_exit = is_exit;
+  this.isVisited = false;
 }
 
 function Pawn(Y_position, X_position){
@@ -65,6 +66,8 @@ function labirynth(str){
         newField = new Field(false, false, true);
         // console.log('$: newField', newField)
         oneMazeRow.push(newField);
+      } else {
+        console.log('Bledny znakd', str[i])
       }
     }  
   }
@@ -143,6 +146,9 @@ function drawTable(arr){
         tableData.className = 'noWall'; 
         tableData.id = yxStr; 
       }
+      if (arr[y][x].isVisited) {
+        tableData.classList.add('visited');
+      }
       table.appendChild(tableRow);
     }  
   }
@@ -153,7 +159,11 @@ function drawPawn(pawn){
   let x = pawn.x.toString();
   let yx = y + ',' + x;
   let findID = document.getElementById(yx);
-  findID.textContent = 'PGG';
+  let img = document.createElement('img');
+  img.src = 'pictures/pawn.jpg';
+  img.className = 'pawnDog';
+  findID.appendChild(img);
+  // findID.textContent = 'PGG';
 }
 
 function showMazeAndControls(){
@@ -198,6 +208,84 @@ function randomMove(positionsArr){
   return positionsArr[randomPos];
 }
 
+// function logPositions(posArr) {
+//   console.log('positionsArr [{');
+//   for (let i = 0; i < posArr.length; i++) {
+//     console.log('    ', posArr[i]);
+//   }
+//   console.log('}');
+// }
+
+function randomMoveNotVisited(positionsArr){
+  logPositions(positionsArr);
+  let selectedPosArr = [];
+  let randomPos = 0;
+
+  while(positionsArr.length > 0) {
+    randomPos = getRandomInt(positionsArr.length);
+    let drawn = positionsArr[randomPos];
+    console.log('drawn', drawn);
+    console.log('pole', mazeModel[drawn[0]][drawn[1]].isVisited);
+    if(mazeModel[drawn[0]][drawn[1]].isVisited === false) {
+      return positionsArr[randomPos];
+    } else if (mazeModel[drawn[0]][drawn[1]].isVisited === true){
+      positionsArr.splice(randomPos, 1);
+      console.log('wycięty', positionsArr)
+      selectedPosArr.push(drawn);
+      console.log('selectedPosArr', selectedPosArr);
+    }  
+  }
+  if(positionsArr.length === 0){
+    randomPos = getRandomInt(selectedPosArr.length);
+    console.log('1st if, randomPos', selectedPosArr[randomPos]);
+    return selectedPosArr[randomPos];
+  }
+}
+
+function randomMoveNotVisited2(positionsArr) {
+  let positionsNotVisited = [];
+  let index = -1;
+  for(let i = 0; i < positionsArr.length; i++){
+    let position = positionsArr[i];
+    if(mazeModel[position[0]][position[1]].isVisited === false){
+      positionsNotVisited.push(position);
+    }
+  }
+  if(positionsNotVisited.length > 0){
+     index = getRandomInt(positionsNotVisited.length);
+    return positionsNotVisited[index];
+  } else {
+    index = getRandomInt(positionsArr.length);
+    return positionsArr[index];
+  }
+}
+
+
+function randomeMoveWithBackTracking(positionsArr){
+  let positionsNotVisited = [];
+  let index = -1;
+  for(let i = 0; i < positionsArr.length; i++){
+    let position = positionsArr[i];
+    if(mazeModel[position[0]][position[1]].isVisited === false){
+      positionsNotVisited.push(position);
+    }
+  }
+  if(positionsNotVisited.length > 0){
+    let currentPosition = [];
+    currentPosition.push(pawn.y, pawn.x);
+    console.log('currentPosition', currentPosition);
+    backTrackingStack.push(currentPosition);
+    
+    index = getRandomInt(positionsNotVisited.length);
+    return positionsNotVisited[index];
+  } else if (backTrackingStack.length === 0){
+    return null;
+  } else {
+    let removed = backTrackingStack.pop()
+    console.log('removed', removed);
+    return removed;
+  }
+}
 
 
 function scrollDownToView() {
@@ -206,38 +294,58 @@ function scrollDownToView() {
   elmnt[0].scrollIntoView();
 }
 
-function proceedRandomlyToExit(){
-  let count = 0;
+
+
+function stopFunction(){
+  isStopped = true;
+  console.log('isStopped', isStopped);
+}
+
+
+function proceedRandomlyToExit(moveSelectionFunction){
+  if (isAlgoritmRunning){
+    alert('Użyj przycisku stop.');
+    return;
+  } 
+
   let isExit = isPawnAtExit(mazeModel, pawn.y,pawn.x);
   let delay = 50;
+  isStopped = false;
+  isAlgoritmRunning = true;
 
   let stepFunction = function() {
-    console.log('stepFunction');
     let posArr = validPositions(mazeModel);
-    let randomPosition = randomMove(posArr);
-    pawn.moveToNewPosition(randomPosition[0], randomPosition[1]); 
-    isExit = isPawnAtExit(mazeModel, pawn.y,pawn.x);
-    count += 1
-    drawEverything(mazeModel, pawn); 
-    if(isExit !== true){
-      setTimeout(stepFunction, delay);
+    let nextPosition = moveSelectionFunction(posArr);
+    mazeModel[pawn.y][pawn.x].isVisited = true;
+    if (nextPosition === null){
+      alert('Przykro mi. Ten algorytm nie działa. Użyj innego.');
+      isAlgoritmRunning = false;
+      return;
     }
-  };
+    pawn.moveToNewPosition(nextPosition[0], nextPosition[1]); 
+    // console.log('nextPosition', nextPosition);
+    isExit = isPawnAtExit(mazeModel, pawn.y,pawn.x);
+    drawEverything(mazeModel, pawn); 
+    if(isExit !== true && isStopped === false){
+      setTimeout(stepFunction, delay);
+    } else {
+      isAlgoritmRunning = false;
+    }
+  }
 
   if(isExit !== true){
     setTimeout(stepFunction, delay);
   }
-
-  console.log('count', count);
 }
 
+let isAlgoritmRunning = false;
+let backTrackingStack = [];
+let isStopped = false; 
 var mazeModel, entrance, pawn;
 
 function createLabirynth(event){
   event.preventDefault();
 
-  let width_input = event.target.width.value;
-  let height_input = event.target.height.value;
   let maze_input = event.target.labirynt.value;
 
   mazeModel = labirynth(maze_input);
@@ -246,20 +354,22 @@ function createLabirynth(event){
   drawEverything(mazeModel, pawn);
   showMazeAndControls();
   scrollDownToView();
-}
 
-addEventListener('submit', createLabirynth);
+  isAlgoritmRunning = false;
+  backTrackingStack = [];
+  isStopped = false; 
+}
 
 
 function movePawn(event){
   let arrow = event.target.id;
   let newCoordinates = pawn.getNewPosition(arrow);
-  let isValid = isPawnPositionValid(mazeModel, newCoordinates[0],newCoordinates[1]);
+  let isValid = isPawnPositionValid(mazeModel, newCoordinates[0], newCoordinates[1]);
   if (isValid === false){
     alert("Gdzie leziesz? Zawróć!")
   }else{ 
     pawn.moveToNewPosition(newCoordinates[0], newCoordinates[1]);
-    }
+  }
   let isAtExit = isPawnAtExit(mazeModel, pawn.y,pawn.x);
   if(isAtExit === true){
     setTimeout(function () {alert('Hurra!! Koniec labiryntu');}, 100);
@@ -268,20 +378,41 @@ function movePawn(event){
 }
 
 function movePawnRandomly(event){
-  proceedRandomlyToExit()
+  proceedRandomlyToExit(randomMove);
 }
+function movePawnRandomCrumbs(event){
+  proceedRandomlyToExit(randomMoveNotVisited2);
+}
+function movePawnRandomCrumbsDFS(event){
+  backTrackingStack = [];
+  proceedRandomlyToExit(randomeMoveWithBackTracking);
+}
+
+function stoppingFuntion(event){
+  stopFunction();
+}
+
+
 
 let buttopUp = document.getElementById('up');
 let buttopDown = document.getElementById('down');
 let buttonLeft = document.getElementById('left');
 let buttonRight = document.getElementById('right');
-let buttonRandom = document.getElementById('random')
+let buttonRandom = document.getElementById('random');
+let buttonCrumbs = document.getElementById('crumbs');
+let buttonThread = document.getElementById('thread');
+let buttonStop = document.getElementById('stop');
+
+addEventListener('submit', createLabirynth);
 
 buttopUp.addEventListener('click', movePawn);
 buttopDown.addEventListener('click', movePawn);
 buttonLeft.addEventListener('click', movePawn);
 buttonRight.addEventListener('click', movePawn);
 buttonRandom.addEventListener('click', movePawnRandomly);
+buttonCrumbs.addEventListener('click', movePawnRandomCrumbs);
+buttonThread.addEventListener('click', movePawnRandomCrumbsDFS);
+buttonStop.addEventListener('click', stoppingFuntion);
 
 
 
